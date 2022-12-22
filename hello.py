fe264ba51d5f6273531706fb7f316c4eec353d10
PBhartiS <bhartisharma.pragya@gmail.com>
2022-12-22 12:05:10 +0530

    add function up

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
 print("hello world")
 print("this is my first program with git")
+print('pushed to git using token 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')
+#now adding new Feature with functions
+def up(s1):
+    s1 = s1.upper()
+    return s1
+output=up("pragya")
+print(output)
